b2193962a97b4a6f0ff5e35644cff3079a0dbfa4
StYoukii <gcottin26@gmail.com>
2018-06-29 20:39:11 +0200

    Mise en place des bases du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreateProfileComponent } from './profile/create-profile/create-profile.component';
+import { LoggedComponent } from './home/logged/logged.component';
+import { NotLoggedComponent } from './home/not-logged/not-logged.component';
+import { EventComponent } from './event/event/event.component';
+import { CreateEventComponent } from './event/create-event/create-event.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ProfileComponent,
+    CreateProfileComponent,
+    LoggedComponent,
+    NotLoggedComponent,
+    EventComponent,
+    CreateEventComponent,
+    SearchBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/not-logged/not-logged.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-logged',
+  templateUrl: './not-logged.component.html',
+  styleUrls: ['./not-logged.component.less']
+})
+export class NotLoggedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
